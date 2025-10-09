62505fad91b0aeda4d3e5ab7543488fb819c630f
LiamAllbee <yoshilink1@gmail.com>
2025-10-08 22:04:53 -0400

    Add chapter 1 and 2

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 88.6;
+var firstName = "Liam";
+var lastName = 'Allbee';
+console.log(found);
+console.log(grade);
+console.log("Hi " + firstName);
+console.log("Hello ".concat(firstName, " ").concat(lastName));
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,10 @@
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Liam";
+let lastName: string = 'Allbee';
+
+console.log(found);
+console.log(grade);
+console.log("Hi " + firstName);
+console.log(`Hello ${firstName} ${lastName}`); 
+//Template strings require the back tick ^^^
